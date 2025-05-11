e7a518d6f1036d558265ad68cdd9d69c1bf703d0
Ayush Dhar Diwan <ayushdhardiwan27@gmail.com>
2025-05-11 13:17:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    > >                     <html lang="en" className="dark">
    > >                       {" "}
    >                         ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (30:5) @ RootLayout
    >
    >   28 | }>) {
    >   29 |   return (
    > > 30 |     <html lang="en" className="dark"> {/* Enforce dark mode */}
    >      |     ^
    >   31 |       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
    >   32 |         <AuthProvider>
    >   33 |           <Navbar />
    >
    > Call Stack
    > 14
    >
    > Show 12 ignore-listed frame(s)
    > html
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (30:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> {/* Enforce dark mode */}
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <AuthProvider>
           <Navbar />
